7430de423ce7fcf9164a8ae9c9289e01f0eb7d1c
Shrishti gupta <shrishtigupta2812@gmail.com>
2026-05-19 19:39:26 +0530

    add sorting feature

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,18 +5,33 @@ import Card from "../components/Card";
 
 function Home({searchTerm, category, setCategory}){
     const [products,setProducts]=useState([]);
+    const [sortOption, setSortOption] = useState("");
      const Categories=["all",...new Set(products.map((item)=>item.category ))];
-  console.log(Categories);
+ 
  const filterItems= products.filter((item)=>{
 const search =searchTerm.trim().toLowerCase()||" ";
   const matchSearch=item.title.toLowerCase().includes(search.toLowerCase())
   || item.description.toLowerCase().includes(search.toLowerCase())  
 
   const matchCategory=category==="all"|| item.category ===category ;
-  return matchSearch && matchCategory;
+ 
+  return matchSearch && matchCategory  ;
 });
   
- 
+  const sortedItems=[...filterItems].sort((a,b)=>{
+    if(sortOption==="Low-High"){
+      return a.price-b.price;
+    }
+    else if(sortOption==="High-Low"){
+      return b.price-a.price;
+    }else if(sortOption==="A-Z"){
+      return a.title.localeCompare(b.title);
+    }else if(sortOption==="Z-A"){
+      return b.title.localeCompare(a.title);
+    }
+      return 0;
+  }
+  )
     async function fetchData() {
   try {
     const response = await fetch('https://dummyjson.com/products');
@@ -43,11 +58,19 @@ fetchData();
            ))}
           </div>
         <div className="mx-auto bg-blue-200 p-4">
-         
-            <h2 className="p-2 text-3xl">Product Gallery</h2>
+         <div className="flex justify-between items-center mb-6">
+                      <h2 className="p-2 text-3xl">Product Gallery</h2>
+                      <select value={sortOption} onChange={(e) => setSortOption(e.target.value)} className="p-2 border rounded-md">
+                        <option>Sort By</option>
+                        <option>Low-High</option>
+                        <option>High-Low</option>
+                        <option>A-Z</option>
+                        <option>Z-A</option>
+                      </select>
+                      </div>
 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 p-4">
-  {filterItems.length > 0 ? ( 
-    filterItems.map((product) => (
+  {sortedItems.length > 0 ? ( 
+    sortedItems.map((product) => (
       <Card key={product.id} product={product} />
     ))
   ) : searchTerm ? (
